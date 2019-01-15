9816c42e28367cf4de32b8702d9c41d13ec74219
Stefan Regniet <s.regniet@techdivision.com>
2019-01-15 13:18:58 +0100

    Modifyi Google Analytics test to include check for correct id

--- Modules/Tests/GoogleAnalytics.ts
+++ Modules/Tests/GoogleAnalytics.ts
@@ -9,8 +9,14 @@ export class GoogleAnalytics extends AbstractTest{
     runTest(page, request) {
         if (request.url().match(/https:\/\/www\.google-analytics\.com\/r\/collect/)) {
             const urlToTest = new URLParse(request.url(), true);
+            const analyticsId = this.getConfiguration().id;
+            // check if is anonymized
             if (!_.has(urlToTest, 'query.aip') || parseInt(_.get(urlToTest, 'query.aip'), 10) !== 1) {
-                this.crawlingResults["Anonimize"] = "Missing anonymize parameter";
+                this.crawlingResults["Anonymize"] = "Missing anonymize parameter";
+            }
+            // check for correct analytics-id
+            if (analyticsId.length > 0 && (!_.has(urlToTest, 'query.tid') || _.get(urlToTest, 'query.tid') !== analyticsId)) {
+                this.crawlingResults["AnalyticsId"] = "Wrong Analytics-Id: " + _.get(urlToTest, 'query.tid') + " instead of " + analyticsId;
             }
         }
     }
