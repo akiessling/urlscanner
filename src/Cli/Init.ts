5b75959f2161ef01f782dbe8b9c8885f46783a85
Andreas Kiessling <andreas@powered-by-coffee.de>
2019-01-19 23:37:02 +0100

    modify crawler options before request, e.g. to set a custom userAgent

--- src/Cli/Init.ts
+++ src/Cli/Init.ts
@@ -1,23 +1,24 @@
 import * as fs from "fs";
 import * as process from "process";
 import * as path from "path";
-import {Argv} from "yargs";
+import { Argv } from "yargs";
 
-export const command = 'init';
-export const desc = 'Copy example configuration file to current directory';
+export const command = "init";
+export const desc = "Copy example configuration file to current directory";
 export const builder = (yargs: Argv) =>
-    yargs.usage('Usage: $0 run [Options]')
-        .option(
-            'config', {
-                alias: 'c',
-                description: 'config file to generate',
-                default: 'configuration.yaml'
-            });
+  yargs.usage("Usage: $0 run [Options]").option("config", {
+    alias: "c",
+    description: "config file to generate",
+    default: "configuration.yaml"
+  });
 
 export function handler(argv) {
-    if (fs.existsSync((path.resolve(process.cwd(), argv.config)))) {
-        console.log(argv.cofig, "exists, skipping");
-    } else {
-        fs.copyFileSync(path.resolve(__dirname, '../../configuration.yaml.dist'), argv.config);
-    }
+  if (fs.existsSync(path.resolve(process.cwd(), argv.config))) {
+    console.log(argv.config, "exists, skipping");
+  } else {
+    fs.copyFileSync(
+      path.resolve(__dirname, "../../configuration.yaml.dist"),
+      argv.config
+    );
+  }
 }
