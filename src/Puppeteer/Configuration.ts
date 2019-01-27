97470accc5306af22485cf8b125bbd82f9cb5f97
Andreas Kiessling <andreas@powered-by-coffee.de>
2019-01-27 18:02:08 +0100

    rename internal configuration key

--- src/Puppeteer/Configuration.ts
+++ src/Puppeteer/Configuration.ts
@@ -1,24 +1,24 @@
 import * as _ from "lodash";
 import { Page } from "puppeteer";
 
-interface ConfigurationInterface {
+interface CrawlerConfigurationInterface {
   maxDepth? : number,
   options? : object
 }
 
 export class Configuration {
-  public configuration: ConfigurationInterface;
+  public crawlerConfiguration: CrawlerConfigurationInterface;
   constructor(public allConfiguration) {
-    this.configuration = _.get(allConfiguration, "crawler", {});
+    this.crawlerConfiguration = _.get(allConfiguration, "crawler", {});
   }
 
   applyOptions(options) {
-    const overrideOptions = _.get(this.configuration, "options", {});
+    const overrideOptions = _.get(this.crawlerConfiguration, "options", {});
     Object.assign(options, overrideOptions);
     return options;
   }
 
   get maxDepth() {
-    return this.configuration.maxDepth || 0;
+    return this.crawlerConfiguration.maxDepth || 0;
   }
 }
